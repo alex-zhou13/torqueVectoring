5e473f0d0b7263baca2455b1688ce36659a63de4
giacomocoraluppi <91134945+giacomocoraluppi@users.noreply.github.com>
2023-03-26 18:45:49 -0400

    Uploading Arduino Nano accelerometer code
    Data sent from accelerometer to arduino nano to esp32s3

---
+++ ArduinoNanoReceiver/main/accel.c
@@ -0,0 +1,51 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "string.h"
+#include "driver/gpio.h"
+
+static const int RX_BUF_SIZE = 1024;
+
+#define TXD_PIN (GPIO_NUM_18)
+#define RXD_PIN (GPIO_NUM_17)
+
+#define UART UART_NUM_2
+
+int num = 0;
+
+void init(void) {
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    uart_driver_install(UART, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART, &uart_config);
+    uart_set_pin(UART, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
+static void rx_task(void *arg)
+{
+    static const char *RX_TASK_TAG = "RX_TASK";
+    esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
+    while (1) {
+        const int rxBytes = uart_read_bytes(UART, data, RX_BUF_SIZE, 300 / portTICK_PERIOD_MS);
+        if (rxBytes > 0) {
+            data[rxBytes] = '\0';
+            ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
+        }
+    }
+    free(data);
+}
+
+void app_main(void)
+{
+    init();
+    xTaskCreate(rx_task, "uart_rx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
+}

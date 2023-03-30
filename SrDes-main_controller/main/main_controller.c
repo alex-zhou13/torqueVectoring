3a38c730757ea76aef89b2d74cb212c87dd51151
alex-zhou13 <75346304+alex-zhou13@users.noreply.github.com>
2023-03-29 20:10:47 -0400

    modified main controller for new aux inputs

--- SrDes-main_controller/main/main_controller.c
+++ SrDes-main_controller/main/main_controller.c
@@ -26,7 +26,7 @@
 #include <string.h>
 
 /////////////// GPIO DEF ////////////////
-#define RELAY_GPIO 19 // Changed to 19
+#define RELAY_GPIO 1 // Default GPIO 1
 
 /////////////// VEHIC PARAMS DEF //////////////
 #define SIM_WHEEL_DIAM  0.5     // m
@@ -42,16 +42,16 @@
 #define SIM_WIDTH       1.5
 
 /////////////// I2C DEF ////////////////
-#define SDA_PIN 9
-#define SCL_PIN 10
+#define SDA_PIN 18
+#define SCL_PIN 19
 #define I2C_CLK_SPEED 400000 // 400 KHz
 
 #define MCP4725_LEFT_ADDRESS 0x62 // default address
 #define MCP4725_MAX_TICKS (10/portTICK_PERIOD_MS) // max ticks for i2c
 
 /////////////// UART DEF ////////////////
-#define TXD GPIO_NUM_17 // Send
-#define RXD GPIO_NUM_18 // Recieve 
+#define TXD GPIO_NUM_16 // Send
+#define RXD GPIO_NUM_17 // Recieve 
 #define UART_RTS (UART_PIN_NO_CHANGE)
 #define UART_CTS (UART_PIN_NO_CHANGE)
 
@@ -68,11 +68,20 @@ int len_out = 4;
 // static xQueueHandle dac_left_evt_queue_send = NULL;
 
 /////////////// GLOBAL VARS DEF ////////////////
-double throttle_scaled = 0;
+double throttle_scaled = 1;
 double thr_left_scaled = 0;
 double thr_rigt_scaled = 0;
-double steering_scaled = 0;
-double speed_scaled = 0; // as input, where do we get these?
+double steering_scaled = 2;
+double speed_scaled = 3; // as input, where do we get these?
+
+// input signals from UART
+char* accel_x;
+char* accel_y;
+char* accel_z;
+char* steer;
+char* throttle;
+
+double something = 0;
 
 ///////////// PI funcs ///////////////////////////
 /* Block signals (default storage) */
@@ -842,33 +851,64 @@ static void uart_init() {
     return;
 }
 
+void split_rx(char input[]) {
+  char *token;
+  char tokens[5][20];
+  int i = 0;
+  
+  token = strtok(input, ",");
+  while (token != NULL) {
+      strcpy(tokens[i], token);
+      token = strtok(NULL, ",");
+      i++;
+  }
+  accel_x = tokens[0];
+  accel_y = tokens[1];
+  accel_z = tokens[2];
+  steer = tokens[3];
+  throttle = tokens[4];
+  printf("%s\n", accel_x);
+  printf("%s\n", accel_y); 
+  printf("%s\n", accel_z); 
+  printf("%s\n", steer); 
+  printf("%s\n", throttle);
+}
+
 static void uart_rx_task() {
     // Configure a temporary buffer for the incoming data
     // uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
     // Buffer for input data
-    uint8_t *data_in = (uint8_t *) malloc(BUF_SIZE);
+    static const char *RX_TASK_TAG = "RX_TASK";
+    char *data_in = (char *) malloc(BUF_SIZE);
     while (1) {
-        int len_in = uart_read_bytes(UART_PORT_NUM, data_in, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
+        int len_in = uart_read_bytes(UART_PORT_NUM, data_in, (BUF_SIZE - 1), 100 / portTICK_PERIOD_MS);
         if (len_in > 0) {
-            if (data_in[0] == start) {
-                int throttle_mv_lower = data_in[1];
-                int throttle_mv_upper = data_in[2];
-                int steering_mv_lower = data_in[3];
-                // int steering_mv_lower = 0;
-                int steering_mv_upper = data_in[4];
-                // int steering_mv_upper = 0;
-                
-                data_in[len_in] = '\0';
-                int throttle_mv = throttle_mv_upper*256+throttle_mv_lower;
-                int steering_mv = steering_mv_upper*256+steering_mv_lower;
-                throttle_scaled = (double) throttle_mv/5000;
-                steering_scaled = (double) steering_mv/5000 - 0.5;
+          data_in[len_in] = '\0';
+          ESP_LOGI(RX_TASK_TAG, "Read %d bytes: \n'%s'", len_in, data_in);
+          split_rx(data_in);
+          something = (double)*accel_x;
+          printf("Something: %f\n", something);
+            // if (data_in[0] == start) {
+            //     int throttle_mv_lower = data_in[1];
+            //     int throttle_mv_upper = data_in[2];
+            //     int steering_mv_lower = data_in[3];
+            //     // int steering_mv_lower = 0;
+            //     int steering_mv_upper = data_in[4];
+            //     // int steering_mv_upper = 0;
 
-                // printf("UART data Recieved: th_left=%d\th_right=%d (mV) \n", throttle_mv, steering_mv);
-                // printf("Values converted:   throttle=%f\tsteering=%f\n", throttle_scaled, steering_scaled);
-                // // Notify PID
-                // xQueueSend(uart_evt_queue_send, &mv_thr_send, 10);
-            }
+            //     printf("First value: $.2d", throttle_mv_lower);
+                
+            //     data_in[len_in] = '\0';
+            //     int throttle_mv = throttle_mv_upper*256+throttle_mv_lower;
+            //     int steering_mv = steering_mv_upper*256+steering_mv_lower;
+            //     throttle_scaled = (double) throttle_mv/5000;
+            //     steering_scaled = (double) steering_mv/5000 - 0.5;
+
+            //     // printf("UART data Recieved: th_left=%d\th_right=%d (mV) \n", throttle_mv, steering_mv);
+            //     // printf("Values converted:   throttle=%f\tsteering=%f\n", throttle_scaled, steering_scaled);
+            //     // // Notify PID
+            //     // xQueueSend(uart_evt_queue_send, &mv_thr_send, 10);
+            // }
         }
         vTaskDelay(pdMS_TO_TICKS(5));
     }
@@ -905,7 +945,8 @@ static void temp_PID_task() {
 
         // Notify dac_left
         // xQueueSend(dac_left_evt_queue_send, &mv_thr_send, 10);
-        vTaskDelay(pdMS_TO_TICKS(10));
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        printf("Linear Left: %.2f, linear right: %.2f\n", thr_left_scaled, thr_rigt_scaled);
     }
     
     Linear_Model_terminate();
@@ -937,8 +978,8 @@ void app_main() {
     gpio_init();
     uart_init();
 
-    xTaskCreate(mcp4725_task,"mcp4725_task",2048,NULL,5,NULL);
+    // xTaskCreate(mcp4725_task,"mcp4725_task",2048,NULL,5,NULL);
     xTaskCreate(uart_rx_task,"uart_rx_task",2048,NULL,5,NULL);
-    xTaskCreate(temp_PID_task,"temp_PID_task",2048,NULL,5,NULL);
-    xTaskCreate(speed_emulator_task,"speed_emulator_task",2048,NULL,5,NULL);
+    // xTaskCreate(temp_PID_task,"temp_PID_task",2048,NULL,5,NULL);
+    // xTaskCreate(speed_emulator_task,"speed_emulator_task",2048,NULL,5,NULL);
 }

371de88b234cdc6961ccee8d6fba48f3f57d4cd5
Alex Zhou <75346304+alex-zhou13@users.noreply.github.com>
2023-04-11 11:25:09 -0400

    Adding first phase of canbus dev

---
+++ SrDes-canbus/main/canbus.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/twai.h"
+
+#define TX_GPIO_NUM     (GPIO_NUM_17)
+#define RX_GPIO_NUM     (GPIO_NUM_18)
+
+void app_main()
+{
+    //Initialize configuration structures using macro initializers
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(TX_GPIO_NUM, RX_GPIO_NUM, TWAI_MODE_NORMAL);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    //Install TWAI driver
+    if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
+        printf("Driver installed\n");
+    } else {
+        printf("Failed to install driver\n");
+        return;
+    }
+
+    //Start TWAI driver
+    if (twai_start() == ESP_OK) {
+        printf("Driver started\n");
+    } else {
+        printf("Failed to start driver\n");
+        return;
+    }
+
+    //Configure message to transmit//////////////////////////////////////
+    twai_message_t message;
+    message.identifier = 0xAAAA;
+    message.extd = 1;
+    message.data_length_code = 4;
+    for (int i = 0; i < 4; i++) {
+        message.data[i] = 0;
+    }
+
+    //Queue message for transmission
+    if (twai_transmit(&message, pdMS_TO_TICKS(1000)) == ESP_OK) {
+        printf("Message queued for transmission\n");
+    } else {
+        printf("Failed to queue message for transmission\n");
+    }
+
+    // //Wait for message to be received////////////////////////////////////
+    // twai_message_t message;
+    // if (twai_receive(&message, pdMS_TO_TICKS(10000)) == ESP_OK) {
+    //     printf("Message received\n");
+    // } else {
+    //     printf("Failed to receive message\n");
+    //     return;
+    // }
+
+    // //Process received message
+    // if (message.extd) {
+    //     printf("Message is in Extended Format\n");
+    // } else {
+    //     printf("Message is in Standard Format\n");
+    // }
+    // printf("ID is %d\n", message.identifier);
+    // if (!(message.rtr)) {
+    //     for (int i = 0; i < message.data_length_code; i++) {
+    //         printf("Data byte %d = %d\n", i, message.data[i]);
+    //     }
+    // }
+
+    //Reconfigure alerts to detect Error Passive and Bus-Off error states////////////
+    uint32_t alerts_to_enable = TWAI_ALERT_ERR_PASS | TWAI_ALERT_BUS_OFF;
+    if (twai_reconfigure_alerts(alerts_to_enable, NULL) == ESP_OK) {
+        printf("Alerts reconfigured\n");
+    } else {
+        printf("Failed to reconfigure alerts");
+    }
+
+    //Block indefinitely until an alert occurs
+    uint32_t alerts_triggered;
+    twai_read_alerts(&alerts_triggered, portMAX_DELAY);
+
+}
